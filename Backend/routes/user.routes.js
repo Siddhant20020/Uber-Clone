2366af8d9674c054daf902b85eecd201b2640a4c
Siddhant Rai <105770390+Siddhant20020@users.noreply.github.com>
2024-12-10 14:15:35 +0545

    User Authentication

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,21 +1,25 @@
-const express=require('express');
-const router=express.Router();
-const {body}=require('express-validator');
-const  userController=require('../controllers/user.controller');
-router.post('/register',[
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator');
+const userController = require('../controllers/user.controller');
+const authMiddleware=require('../middlewares/auth.middleware');
+router.post('/register', [
   body('email').isEmail().withMessage('Invalid Email'),
-  body('fullname.firstname').isLength({min:3}).withMessage("First Name must be at least 3 character"),
-  body('password').isLength({min:6}).withMessage('Password must be at least 6 character'),
+  body('fullname.firstname').isLength({ min: 3 }).withMessage("First Name must be at least 3 character"),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 character'),
 ],
-userController.registerUser)
+  userController.registerUser)
 
-router.post('/login',[
+router.post('/login', [
   body('email').isEmail().withMessage('Invalid Email'),
-  body('password').isLength({min:6}).withMessage("Password must be at least 6 characters")
+  body('password').isLength({ min: 6 }).withMessage("Password must be at least 6 characters")
 ],
-userController.loginUser
+  userController.loginUser
 )
+router.get('/profile', authMiddleware.authUser,userController.getUserProfile);
+
+router.get('/logout',authMiddleware.authUser,userController.logoutUser);
 
 
 
-module.exports=router;
+module.exports = router;
